ffba884e164506a9aabb3ab3b7ea24bba71e0d50
Eric Marsh <ericmrsh9@gmail.com>
2022-01-21 16:52:53 -0800

    enviorment variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+// require('dotenv').config({path:'.env.development'});
+require('dotenv').config({ path: '.env.production' });
+
 
 module.exports = {
   siteMetadata: {
